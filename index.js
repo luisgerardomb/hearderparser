342351d79c16008c44bac9d9e2ceb7a39bc3c9b0
lgmarzoa <marzoalg@sanborns.com.mx>
2023-10-13 12:48:19 -0600

    first

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  console.log(req.headers, req.ip);
+
+  const jsonData = {
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  }
+
+  res.json(jsonData);
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
